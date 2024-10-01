405c2958a60e6c8e5bf2164f46f66ef47baa3b3e
79283 <792836909@qq.com>
2024-10-01 17:32:20 +0800

    基础py语法

---
+++ Unit1注释与输出.py
@@ -0,0 +1,19 @@
+
+
+'''
+多行注释
+'''
+
+# 单行注释
+
+# ZeroDivisionError: division by zero  ZeroDivisionError：除以零
+# print(1/0)
+
+print("hello")
+
+# print函数
+# 多个参数 sep：分隔符，end：结束符，file：文件对象，flush：是否刷新缓冲区
+# print(*object, sep=" ", end="\n", file=sys.stdout, flush=False)
+print('hello1','hello2','hello3')
+print('hello1','hello2','hello3',sep='@')
+print('hello1',end=" 1008611")
--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-# 导入selenium模块中的webdriver子模块
 from selenium import webdriver
-
-# 导入selenium模块中的chrome子模块的Service类
 from selenium.webdriver.chrome.service import Service
-
-# 导入selenium模块中的By类
 from selenium.webdriver.common.by import By
-
-# 导入time模块，用于等待页面加载
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
+import random
+
+
+# 定义一个函数用于确保元素在视口内
+def scroll_to_element(driver, element):
+    driver.execute_script("arguments[0].scrollIntoView(true);", element)
+    time.sleep(0.5)  # 等待滚动完成
+
+
+# 定义一个函数用于模拟自然的鼠标移动
+def move_to_element_naturally(driver, element):
+    actions = ActionChains(driver)
+
+    # 确保元素在视口内
+    scroll_to_element(driver, element)
+
+    # 获取元素的位置和大小
+    x_offset = element.location['x'] + element.size['width'] / 2  # 目标元素中心X坐标
+    y_offset = element.location['y'] + element.size['height'] / 2  # 目标元素中心Y坐标
+
+    # 移动到目标元素
+    actions.move_to_element(element).perform()  # 直接移动到元素
+
+    time.sleep(random.uniform(0.1, 0.3))  # 等待随机时间
+    actions.click().perform()  # 点击元素
+
 
 # 定义一个print_hi函数
 def print_hi():
-    # 打印输出 'Hello, World!'
     print('Hello, World!')
 
     # 设置ChromeDriver的路径
@@ -25,18 +46,22 @@ def print_hi():
     driver = webdriver.Chrome(service=service)
 
     # 打开指定的URL网页
-    #driver.get('https://www.baidu.com')
     driver.get('https://www.ymt.com/')
 
     # 等待页面加载
     time.sleep(2)
 
-    # Click on the specified element
+    # 找到指定的元素
     element = driver.find_element(By.XPATH, '//a[@href="/hangqing"]')
-    element.click()
 
-    # 等待搜索结果加载
-    time.sleep(2)
+    # 模拟自然鼠标移动并点击
+    move_to_element_naturally(driver, element)
+
+    # 等待页面跳转
+    time.sleep(3)
+
+    # 确保页面加载完成，可以通过查找新的元素
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'h3')))
 
     # 获取所有h3标签的文本内容
     titles = driver.find_elements(By.TAG_NAME, 'h3')
@@ -48,6 +73,8 @@ def print_hi():
     # 关闭浏览器
     driver.quit()
 
+
 # 如果当前文件被直接执行，则调用print_hi函数
 if __name__ == '__main__':
     print_hi()
+
